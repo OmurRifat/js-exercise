7a6237bfca73e277bef34f284be948b676589992
OmurRifat <codemen.jabed@gmail.com>
2024-01-08 19:38:29 +0600

    BinaryGap problem solved

---
+++ Lesson-1/BinaryGap.js
@@ -0,0 +1,18 @@
+function solution(decimal) {
+    // to convert positive decimal to binary
+    let binary = decimal.toString(2);
+    let maxLength = 0;
+    let updatedBinary = binary.split("1");
+    if (updatedBinary[updatedBinary.length - 1] !== "") {
+        updatedBinary.pop()
+    }
+    updatedBinary.forEach(item => {
+        if (item.length > maxLength) {
+            maxLength = item.length
+        }
+    })
+    if (maxLength === 0)
+        return 0;
+    else
+        return maxLength;
+}
